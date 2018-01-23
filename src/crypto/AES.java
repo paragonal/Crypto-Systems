b968780717ae0413913890d2c13c9b0a2f00aefc
FriendlySinusoid <s.f.skarpiphi@gmail.com>
2018-01-23 11:57:04 -0500

    cause

--- src/crypto/AES.java
+++ src/crypto/AES.java
@@ -3,14 +3,13 @@ package crypto;
 import java.util.Arrays;
 
 public class AES {
-	
+
     private int[][][] state;
 
     private int[][] key;
-    private int[][] subkey;
 
     public static void main(String args[]) {
-        AES sys = new AES(new int[][]{{1, 2, 3, 2}, {123, 1, 1, 23}, {123, 1, 1, 23}, {123, 1, 1, 23}}, "Hello there this is a poem, maybe it isn't who really knows?");
+        AES sys = new AES(new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}}, "Hello there this is a poem, maybe it isn't who really knows?");
         System.out.println(sys.digest());
     }
 
@@ -22,21 +21,50 @@ public class AES {
         for (int i = 0; i < blocks; i++)
             for (int j = 0; j < 4; j++)
                 for (int k = 0; k < 4; k++) {
-                    state[i][j][k] = (index < message.length()) ? (int) message.charAt(index) : 255;
+                    state[i][j][k] = (index < message.length()) ? (int) message.charAt(index) : 0;
                     index++;
                 }
+        keySchedule();
     }
 
     public void keySchedule() {
+        int[][] subkey = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            int[] col = new int[4];
+            for (int j = 0; j < 4; j++)
+                subkey[i][j] = key[j][i];
+        }
+        System.out.println(Arrays.deepToString(key));
+        System.out.println(Arrays.deepToString(subkey));
 
+        int ind = 0;
+        int[][] temp = new int[4][4];
+        for(int[] col : subkey){
+            for(int i = 0; i < 4; i++)
+                temp[ind][i] = col[(i+2)%4];
+            ind++;
+        }
+        subkey = temp;
+        System.out.println(Arrays.deepToString(subkey));
+
+
+    }
+
+    public void addRoundKey() {
+        for (int[][] block : state) {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    block[i][j] = block[i][j] ^ key[i][j];
+        }
     }
 
     private void substitute() {
         //substitution
         for (int[][] block : state)
             for (int[] row : block)
-                for (int i = 0; i < 4; i++)
-                    row[i] = Tables.sBox[row[i] - 1];
+                for (int i = 0; i < 4; i++) {
+                    row[i] = Tables.sBox[row[i]];
+                }
     }
 
     private void shift() {
@@ -55,53 +83,51 @@ public class AES {
     private void round() {
         substitute();
         shift();
-        //TODO mix columns
+        mixColumns();
 
-        //TODO diffuse
 
         //TODO some weird block chain key shit
 
 
     }
 
-	public void mixColumns(){
-		for (int n = 0; n < state.length; n++) {
-			int[][] arr = state[n];
-			int[][] temp = new int[4][4];
-			for (int i = 0; i < 4; i++) {
-				System.arraycopy(arr[i], 0, temp[i], 0, 4);
-			}
-			for (int i = 0; i < 4; i++){
-				for (int j = 0; j < 4; j++){
-					arr[i][j] = colHelper(temp, Tables.galois, i, j);
-				}
-			}
-		}
-	}
+    public void mixColumns() {
+        for (int n = 0; n < state.length; n++) {
+            int[][] arr = state[n];
+            int[][] temp = new int[4][4];
+            for (int i = 0; i < 4; i++) {
+                System.arraycopy(arr[i], 0, temp[i], 0, 4);
+            }
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    arr[i][j] = colHelper(temp, Tables.galois, i, j);
+                }
+            }
+        }
+    }
 
-	private int colHelper(int[][] arr, int[][] g, int i, int j)
-	{
-		int colsum = 0;
-		for (int k = 0; k < 4; k++) {
-			int a = g[i][k];
-			int b = arr[k][j];
-			colsum ^= colCalc(a, b);
-		}
-		return colsum;
-	}
+    private int colHelper(int[][] arr, int[][] g, int i, int j) {
+        int colsum = 0;
+        for (int k = 0; k < 4; k++) {
+            int a = g[i][k];
+            int b = arr[k][j];
+            colsum ^= colCalc(a, b);
+        }
+        return colsum;
+    }
 
-	private int colCalc(int a, int b) //Helper method for mcHelper
-	{
-		if (a == 1) {
-			return b;
-		} else if (a == 2) {
-			return Tables.mc2[b / 16][b % 16];
-		} else if (a == 3) {
-			return Tables.mc3[b / 16][b % 16];
-		}
-		return 0;
-	}
-    
+    private int colCalc(int a, int b) //Helper method for mcHelper
+    {
+        if (a == 1) {
+            return b;
+        } else if (a == 2) {
+            return Tables.mc2[b / 16][b % 16];
+        } else if (a == 3) {
+            return Tables.mc3[b / 16][b % 16];
+        }
+        return 0;
+    }
+
     public String digest() {
 
         for (int i = 0; i < 10; i++) {
